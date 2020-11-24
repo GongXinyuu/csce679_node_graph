e53b4e665ee861c38dc0f638ae66b90797b5e905
Xinyu Gong <neogong@fb.com>
2020-11-24 13:14:03 -0600

    update tool tips

--- data.js
+++ data.js
@@ -9,8 +9,21 @@ var svg = d3.select("body")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
 //bring in nodes and links
 var nodes = JSON.parse(nodes)
+var nodes_clone = nodes
+
+var index = [];
+
+// build the index
+for (var x in nodes_clone) {
+   index.push(x);
+}
+
 
 var links = JSON.parse(links)
 
@@ -41,6 +54,7 @@ function linkWidth(link){
 //hip hop #0066cc
 //metal #993366
 //indie #ffff80
+
 
 //defines node
 var node = svg
@@ -231,6 +245,118 @@ var node = svg
         console.log(d.genre)
         return "#000000"
     })
+    .on("mouseover", function(event, d) {
+        
+        // Make graph when mouse is over node
+        var songName = d.name
+        var songIndex = 0
+
+        for (var i = 0; i < Object.keys(nodes_clone).length; i++) {
+            if (songName === nodes_clone[i].name) {
+                songIndex = i + 1;
+            }
+        }
+        
+        // Make graph
+        d3.csv("Spotify-200_t2.csv", function(data) {
+            
+            return {
+                attributes: data[0],        // attributes
+                music: data[songIndex]      // index number of music
+            }
+        }).then(function(data) {
+            var title = data[0].music
+            var artist = data[1].music
+            var genre = data[2].music
+            var year = data[3].music
+        
+            d3.csv("Spotify-200_t.csv", function(data) {
+                return {attributes: data[0], music: data[songIndex]}    // 1 is index num
+            }).then(function(data) {
+                data.forEach(function(d) {
+                    d.music = +d.music;
+                });
+                
+                // Add X axis
+                var x = d3.scaleLinear()
+                    .domain([0, 500])
+                    .range([ 0, 300]);
+                svg.append("g")
+                    .attr("transform", "translate(100," + 370 + ")")
+                    .call(d3.axisBottom(x))
+                    .selectAll("text")
+                    .attr("transform", "translate(-10,0)rotate(-45)")
+                    .style("text-anchor", "end")
+                    .attr("id", "xaxis");
+                
+                // Y axis
+                var y = d3.scaleBand()
+                    .range([ 0, 370 ])
+                    .domain(data.map(function(d) { return d.attributes; }))
+                    .padding(1);
+                svg.append("g")
+                    .attr("transform", "translate(100)")
+                    .call(d3.axisLeft(y))
+                    .attr("id", "yaxis");
+                
+                // append the rectangles for the bar chart
+                svg.selectAll(".bar")
+                    .data(data)
+                    .enter()
+                    .append("rect")
+                    .classed("bar", true)
+                    .attr("height", 15)
+                    .attr("width", function(d) {
+                        if (d.music < 0) {
+                            return d.music * -.6;
+                        }
+                        return d.music * .6;
+                    })
+                    .attr("y", (d, i) => 27 + (i * (33.6)))
+                    .attr("x", 100)
+                    .style("fill", "#69b3a2")
+                    .attr("stroke", "black");
+                
+                // Song name
+                svg.append("text")
+                    .attr('transform', `translate(${100}, ${370 + 10 + 35})`)
+                    .attr('text-anchor', 'left')
+                    .attr('font-size', '14px')
+                    .attr('fill', 'black')
+                    .text('Title: ' + title)
+        
+                // Song artist
+                svg.append("text")
+                    .attr('transform', `translate(${100}, ${370 + 10 + 55})`)
+                    .attr('text-anchor', 'left')
+                    .attr('font-size', '14px')
+                    .attr('fill', 'black')
+                    .text('Artist: ' + artist)
+        
+                // Song genre
+                svg.append("text")
+                    .attr('transform', `translate(${100}, ${370 + 10 + 75})`)
+                    .attr('text-anchor', 'left')
+                    .attr('font-size', '14px')
+                    .attr('fill', 'black')
+                    .text('Genre: ' + genre)
+                
+                // Song year
+                svg.append("text")
+                    .attr('transform', `translate(${100}, ${370 + 10 + 95})`)
+                    .attr('text-anchor', 'left')
+                    .attr('font-size', '14px')
+                    .attr('fill', 'black')
+                    .text('Release Year: ' + year)
+            });
+        })
+    })
+    .on("mouseout", function() {
+        d3.selectAll("g").remove();
+        d3.selectAll("text").remove();
+        d3.selectAll("rect").remove();
+        console.log('done');
+    });
 
 //varies the pull of a link based on magnitude
 function linkStr(link){
